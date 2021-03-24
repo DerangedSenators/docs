c1bb4ed5aba6bfed919b10c205d7c90ff5d26e83
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2021-03-24 18:08:02 +0000

    Update getdocs.sh

--- getdocs.sh
+++ getdocs.sh
@@ -24,6 +24,7 @@ doxygen ../../Doxyfile
 ./../../doxygen/doxybook2 --input xml/ --output ../../docs/PlayerLink --config ../../doxygen/doxybookcfg_playerlink.json
 cd ..
 cd copsandrobbers
+find ./CopsAndRobbers -mindepth 1 ! -regex '^./CopsAndRobbers/Assets/Scripts\(/.*\)?' -delete
 doxygen ../../Doxyfile
 ./../../doxygen/doxybook2 --input xml/ --output ../../docs/"Cops And Robbers" --config ../../doxygen/doxybookcfg_copsandrobbers.json
 cd ../..
